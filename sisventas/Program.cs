5196fdbb5244f217cd86650035e07c7e5ee6fe62
jochylaverdad <jochy0360@gmail.com>
2021-09-12 15:05:11 -0400

    [IMP] delete reference

--- sisventas/Program.cs
+++ sisventas/Program.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CapaPresentacion;
+using System;
 using System.Windows.Forms;
-
-
-using CapaPresentacion;
 
 namespace sisventas
 {
